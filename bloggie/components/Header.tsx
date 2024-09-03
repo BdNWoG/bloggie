637d9a11ae2ce95dfc1ca9fb1dc9cf7f30dce43b
BdNWoG <billy.gao@hotmail.com>
2024-09-03 11:32:23 +0800

    assets

--- bloggie/components/Header.tsx
+++ bloggie/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Image from 'next/image'
+import { assets } from '@/assets/assets'
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className='py-5 px-5 md:px-12 lg:px-28'>
+            <div className='flex justify-between items-center'>
+                <Image src={assets.logo} width={180} className='w-[130px] sm:w-auto'/>
+            </div>
         </div>
     )
 }
