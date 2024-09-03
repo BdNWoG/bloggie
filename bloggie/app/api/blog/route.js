915b200d2cb8c71a8c1e829fc3c51c871e6db3b5
BdNWoG <billy.gao@hotmail.com>
2024-09-04 06:48:49 +0800

    Create route.js

---
+++ bloggie/app/api/blog/route.js
@@ -0,0 +1,27 @@
+const NextResponse = require("next/server")
+const { ConnectDB } = require("../../../lib/config/db")
+import { writeFile } from 'fs/promises'
+
+const LoadDB = async () => {
+    await ConnectDB();
+}
+
+LoadDB();
+
+async function GET(request){
+    return NextResponse.json({msg: "API Working"})
+}
+
+export async function POST(request){
+    const formData = await request.formData();
+    const timestamp = Date.now();
+
+    const image = formData.get("image");
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path, buffer);
+    const imgUrl = `/${timestamp}_${image.name}`
+    console.log(imgUrl);
+    return NextResponse.json({imgUrl})
+}
