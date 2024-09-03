97fe3bdbfd4a394ad772b1912db0ba12d76a1c90
BdNWoG <billy.gao@hotmail.com>
2024-09-03 12:26:06 +0800

    blog

--- bloggie/app/page.tsx
+++ bloggie/app/page.tsx
@@ -1,10 +1,13 @@
+'use client'
+
+import BlogList from "@/components/BlogList";
 import Header from "@/components/Header";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <> 
       <Header/>
+      <BlogList/>
     </>
   );
 }
