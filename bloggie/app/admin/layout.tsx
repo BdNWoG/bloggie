750cf89cb8b6d0b7eeb7d57eb6f2fe3045317c8a
BdNWoG <billy.gao@hotmail.com>
2024-09-13 16:02:09 +0800

    admin

--- bloggie/app/admin/layout.tsx
+++ bloggie/app/admin/layout.tsx
@@ -1,4 +1,6 @@
+import { assets } from "@/assets/assets";
 import Sidebar from "@/components/admin/Sidebar";
+import Image from "next/image";
 
 //@ts-ignore
 export default function layout({children}){
@@ -6,8 +8,16 @@ export default function layout({children}){
         <>
             <div className="flex">
                 <Sidebar/>
+                <div className="flex flex-col w-full">
+                    <div className="flex items-center justify-between w-full py-3 max-h-[60px] px-12 border-black border-b">
+                        <h3 className="font-medium">
+                            Admin Panel
+                        </h3>
+                        <Image src={assets.profile_icon} width={40} alt=""/>
+                    </div>
+                    {children}
+                </div>
             </div>
-            {children}
         </>
     )
 }
