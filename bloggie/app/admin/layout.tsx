f8243a6c3cb139a2cffc533c07de996858cbb1c4
BdNWoG <billy.gao@hotmail.com>
2024-09-13 14:16:55 +0800

    admin

--- bloggie/app/admin/layout.tsx
+++ bloggie/app/admin/layout.tsx
@@ -1,7 +1,12 @@
+import Sidebar from "@/components/admin/Sidebar";
+
 //@ts-ignore
 export default function layout({children}){
     return (
         <>
+            <div className="flex">
+                <Sidebar/>
+            </div>
             {children}
         </>
     )
